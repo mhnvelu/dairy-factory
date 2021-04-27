45519129cfed2ba67a5efe4d80019c01b7117da6
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-04-27 18:20:22 +0100

    Loaded bootstrap data

---
+++ src/main/java/com/spring/microservices/dairyfactory/bootstrap/DairyFactoryDataLoader.java
@@ -0,0 +1,35 @@
+package com.spring.microservices.dairyfactory.bootstrap;
+
+import com.spring.microservices.dairyfactory.domain.Butter;
+import com.spring.microservices.dairyfactory.repository.ButterRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class DairyFactoryDataLoader implements CommandLineRunner {
+
+    private final ButterRepository butterRepository;
+
+    public DairyFactoryDataLoader(ButterRepository butterRepository) {
+        this.butterRepository = butterRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Loading Butter...");
+        if (butterRepository.count() == 0) {
+            butterRepository
+                    .save(Butter.builder().name("Butter Honey").weightInGms(100).flavour("Honey")
+                                  .price(new BigDecimal("10.99")).quantityInStock(100).build());
+
+            butterRepository
+                    .save(Butter.builder().name("Butter Garlic").weightInGms(50).flavour("Garlic")
+                                  .price(new BigDecimal("11.99")).quantityInStock(200).build());
+        }
+
+        System.out.println("Loaded Butter : " + butterRepository.count());
+
+    }
+}
