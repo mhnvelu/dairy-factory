18b3bb3c866c537e29ef33c616eb16fc4f2d0cb8
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-05-17 22:35:06 +0100

    Fixed issues

--- src/main/java/com/spring/microservices/dairyfactory/domain/Customer.java
+++ src/main/java/com/spring/microservices/dairyfactory/domain/Customer.java
@@ -2,6 +2,7 @@ package com.spring.microservices.dairyfactory.domain;
 
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -20,6 +21,7 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type = "org.hibernate.type.UUIDCharType")
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID customerId;
 
